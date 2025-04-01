f1b204f09579c1b9da628309f3e3bfda5bb991a8
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2025-04-01 11:55:15 -0300

    inicial

---
+++ react-app/src/Components/ListGroup.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
